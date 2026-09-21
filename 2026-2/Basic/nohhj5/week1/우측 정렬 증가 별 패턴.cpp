4474d00189149edbb163841d13b400562847b1a3
nohhj5 <sshi2014@naver.com>
2026-09-21 17:05:28 +0900

    Create 우측 정렬 증가 별 패턴.cpp

---
+++ 2026-2/Basic/nohhj5/week1/우측 정렬 증가 별 패턴.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+
+    for (int i = 1; i <= N; i++)
+        {
+            for (int j = N-i; j >= 0; j--)
+                {
+                    cout << " ";                    
+                }
+
+            for (int k = 1; k <= i; k++)
+                {
+                    cout << "*";
+                }
+            cout << endl;
+        }
+    return 0;
+}
